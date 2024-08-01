d98241b27145f5a2b2da8ff62683a7c8c78a40f6
Saeid Bostandoust <ssbostan@yahoo.com>
2024-08-01 19:01:37 +0100

    Update jsonify function

--- api/api/util/jsonify.py
+++ api/api/util/jsonify.py
@@ -2,14 +2,20 @@ from flask import current_app
 
 APP_CODES = {
     100: "OK.",
-    101: "Feature is not implemented.",
+    101: "Feature not implemented.",
+    102: "Database error.",
+    103: "Schema instance error.",
+    104: "Input validation error.",
+    105: "Resource not found.",
+    106: "Resource busy.",
 }
 
 
 def jsonify(state={}, metadata={}, status=200, code=100, headers={}):
-    resource = state.copy()
-    resource.update(metadata)
-    resource["code"] = code
+    resource = {}
+    resource["output"] = state
+    resource["metadata"] = metadata
+    resource["status"] = {"code": code}
     if current_app.debug is True:
-        resource["message"] = APP_CODES[code]
+        resource["status"]["message"] = APP_CODES[code]
     return resource, status, headers
