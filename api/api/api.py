8adc0e765128cfcba9fd59dc9a09b43dd7681f8f
Saeid Bostandoust <ssbostan@yahoo.com>
2024-08-12 13:30:23 +0100

    (API) Add config from mapping

--- api/api/api.py
+++ api/api/api.py
@@ -5,9 +5,10 @@ from .object import db, ma, mg
 from .view import apiv1_bp
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config.from_mapping(config)
     db.init_app(app)
     mg.init_app(app, db)
     ma.init_app(app)
